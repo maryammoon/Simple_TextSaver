62f7fcb3fa845bee89208edc1b321893d608e731
maryammoon <144543293+maryammoon@users.noreply.github.com>
2023-10-26 22:55:15 +0330

    bottom navigation

--- app/src/main/java/ir/dunijet/textsaver/Register_Fragment.kt
+++ app/src/main/java/ir/dunijet/textsaver/Register_Fragment.kt
@@ -5,44 +5,55 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import ir.dunijet.textsaver.databinding.FragmentRegisterBinding
 
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [Register_Fragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
 class Register_Fragment : Fragment() {
-    lateinit var binding:FragmentRegisterBinding
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
+    ): View? {
         // Inflate the layout for this fragment
-        binding = FragmentRegisterBinding.inflate(layoutInflater)
-        return binding.root
+        return inflater.inflate(R.layout.fragment_register_, container, false)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        binding.buttomNavigationMain.setOnItemSelectedListener {
-            when(it.itemId){
-                R.id.menu_register->{
-                    replaceFragment(Profile_Fragment())
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment Register_Fragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            Register_Fragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
                 }
-                R.id.menu_profile->{
-                    replaceFragment(Profile_Fragment())
-                }
-                R.id.menu_home->{
-                    replaceFragment(Home_Fragment())
-                }
-        }
-            true
-    }
-        binding.buttomNavigationMain.setOnItemReselectedListener{}
-
-    }
-
-    private fun replaceFragment(fragment: Fragment){
-        val transaction =activity?.supportFragmentManager?.beginTransaction()
-        transaction?.replace(R.id.frame_main_container ,fragment)
-        transaction?.commit()
+            }
     }
 }
