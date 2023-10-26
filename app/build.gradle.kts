d4057010d02b17739f15477e087ccbccb784bfe3
maryammoon <144543293+maryammoon@users.noreply.github.com>
2023-10-26 20:13:28 +0330

    navigation component

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "ir.dunijet.textsaver"
     compileSdk = 33
 
+    buildFeatures{
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "ir.dunijet.textsaver"
         minSdk = 24
@@ -44,4 +48,15 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+//lottie
+    implementation ("com.airbnb.android:lottie:6.1.0")
+
+    //navigationComponent
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.4")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.4")
+
+    //splash screen
+    implementation("androidx.core:core-splashscreen:1.0.1")
+
 }
--- app/src/main/java/ir/dunijet/textsaver/MainActivity.kt
+++ app/src/main/java/ir/dunijet/textsaver/MainActivity.kt
@@ -2,10 +2,14 @@ package ir.dunijet.textsaver
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ir.dunijet.textsaver.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
